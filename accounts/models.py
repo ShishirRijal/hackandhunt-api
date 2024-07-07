22adc1a69d1641ff663ac2773a26b5d9bb5681a2
Shishir Rijal <ccrrizal0892@gmail.com>
2024-07-08 02:34:10 +0545

    create custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(max_length=40, unique=True)
+    name = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
