f3824775189ab273fcde1744a703bda5bb291659
Shishir Rijal <ccrrizal0892@gmail.com>
2024-07-08 03:07:05 +0545

    add custom forms for user creation and update

---
+++ accounts/forms.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + (
+            "name",
+            "is_active",
+            "is_superuser",
+        )
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
