a42fb970b18a278f52ad19d52330ea1222e5fb4c
Shishir Rijal <ccrrizal0892@gmail.com>
2024-07-08 03:28:58 +0545

    add custom useadmin to update admin interface

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,35 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from accounts.forms import CustomUserChangeForm, CustomUserCreationForm
 from accounts.models import CustomUser
 
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ["username", "name", "email", "is_active", "is_superuser"]
+
+    # Define fieldsets for editing existing users in the admin panel
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            None,
+            {
+                "fields": ("name",),
+            },
+        ),
+    )
+
+    # Define fieldsets for adding new users
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            None,
+            {
+                "fields": ("name", "is_active", "is_superuser"),
+            },
+        ),
+    )
+
+
 # Register your models here.
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
