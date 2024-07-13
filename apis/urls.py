d51951a09a539a28f066e4751f2221f7f5d3aaed
Shishir Rijal <ccrrizal0892@gmail.com>
2024-07-13 23:59:49 +0545

    add leaderboard url

--- apis/urls.py
+++ apis/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import LevelViewSet, RiddleViewSet
+from .views import LevelViewSet, RiddleViewSet, LeaderboardViewSet, CurrentLevelViewSet
 
 router = DefaultRouter()
 
 router.register(r"levels", LevelViewSet, basename="level")
 router.register(r"riddles", RiddleViewSet, basename="riddle")
+router.register(r"leaderboard", LeaderboardViewSet, basename="leaderboard")
 
 urlpatterns = router.urls
