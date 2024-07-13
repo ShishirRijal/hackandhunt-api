578ae38dcdccc18f1dae509dba1535a3220e1991
Shishir Rijal <ccrrizal0892@gmail.com>
2024-07-14 00:05:13 +0545

    remvoed redundant model

--- apis/models/riddle.py
+++ apis/models/riddle.py
@@ -2,20 +2,6 @@ from django.db import models
 
 
 class Riddle(models.Model):
-    number = models.IntegerField(null=False, unique=True)
-    question = models.TextField(null=False)
-    answer = models.CharField(null=False, max_length=255)
-    level = models.ForeignKey("Level", on_delete=models.CASCADE)
-    is_trap = models.BooleanField(default=False)
-    code = models.TextField(null=True)
-    image = models.TextField(null=True)
-    hint = models.TextField(null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    from django.db import models
-
-
-class Riddle(models.Model):
     riddle_id = models.CharField(null=False, unique=True, max_length=10)
     question = models.TextField(null=False)
     answer = models.CharField(null=False, max_length=255)
@@ -39,4 +25,4 @@ class Riddle(models.Model):
         super(Riddle, self).save(*args, **kwargs)
 
     def __str__(self):
-        return f"Riddle {self.number}: {self.question}"
+        return f"Riddle {self.riddle_id}: {self.question}"
