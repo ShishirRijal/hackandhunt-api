86dd612b1775703f77a394d56c4de7bb9f80c6aa
Shishir Rijal <ccrrizal0892@gmail.com>
2024-07-13 20:29:56 +0545

    add riddle viewsets

--- apis/views.py
+++ apis/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from apis.models.level import Level
-from apis.serializers.level_serializer import LevelSerializer
+from apis.models import *
+
+
+from apis.serializers import *
 from .permissions import IsSuperUserOrReadOnly
 
 
@@ -10,3 +13,19 @@ class LevelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsSuperUserOrReadOnly]
     queryset = Level.objects.all()
     serializer_class = LevelSerializer
+
+
+class RiddleViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsSuperUserOrReadOnly]
+    queryset = Riddle.objects.all()
+    serializer_class = RiddleSerializer
+
+    # Get riddles according to the level_id
+    def get_queryset(self):
+        level_id = self.request.query_params.get("level_id")
+        if level_id is not None:
+            # Filter by level_id if provided
+            return Riddle.objects.filter(level_id=level_id)
+        else:
+            # Otherwise, return all riddles
+            return Riddle.objects.all()
