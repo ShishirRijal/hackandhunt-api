6655adf6ede3b1dc1bb943f28aa9330e7fa5f444
Shishir Rijal <ccrrizal0892@gmail.com>
2024-07-13 19:07:10 +0545

    Add authorization to levels viewset Only the authenticated users can view the levels Only the superusers can create levels

--- apis/views.py
+++ apis/views.py
@@ -3,8 +3,10 @@ from rest_framework import viewsets
 
 from apis.models.level import Level
 from apis.serializers.level_serializer import LevelSerializer
+from .permissions import IsSuperUserOrReadOnly
 
 
 class LevelViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsSuperUserOrReadOnly]
     queryset = Level.objects.all()
     serializer_class = LevelSerializer
