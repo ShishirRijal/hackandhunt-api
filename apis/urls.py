7a7079d0e16ac8f515fb8f475894bd15aa4e7a5d
Shishir Rijal <ccrrizal0892@gmail.com>
2024-07-14 00:00:45 +0545

    add user-progress url

--- apis/urls.py
+++ apis/urls.py
@@ -6,5 +6,6 @@ router = DefaultRouter()
 router.register(r"levels", LevelViewSet, basename="level")
 router.register(r"riddles", RiddleViewSet, basename="riddle")
 router.register(r"leaderboard", LeaderboardViewSet, basename="leaderboard")
+router.register(r"current-level", CurrentLevelViewSet, basename="current-level")
 
 urlpatterns = router.urls
