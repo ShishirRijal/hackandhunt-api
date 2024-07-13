5181bd4ad3bcb4040b82686ac9a0a3bb77370ce0
Shishir Rijal <ccrrizal0892@gmail.com>
2024-07-13 23:57:04 +0545

    add leaderboard serializer

--- apis/serializers/__init__.py
+++ apis/serializers/__init__.py
@@ -1,7 +1,10 @@
 from .level_serializer import LevelSerializer
 from .riddle_serializer import RiddleSerializer
+from .leaderboard_serializer import LeaderboardSerializer, UserCurrentLevelSerializer
 
 __all__ = [
     "LevelSerializer",
     "RiddleSerializer",
+    "LeaderboardSerializer",
+    "UserCurrentLevelSerializer",
 ]
