3b5692e6e61b707061153b68b1c393c3736f1be3
Shishir Rijal <ccrrizal0892@gmail.com>
2024-07-15 15:29:01 +0545

    add signup

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -8,3 +8,19 @@ class CustomUserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ["id", "username", "email", "name", "is_active", "is_superuser"]
         read_only_fields = ["is_active", "is_superuser"]
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ["username", "email", "name", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            name=validated_data["name"],
+            password=validated_data["password"],
+        )
+        return user
